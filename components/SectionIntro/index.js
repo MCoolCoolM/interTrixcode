8c06ac93b2e683c85e8b1c6f2fada340c6bb8a47
t.daulbaev <linainvers91@gmail.com>
2021-12-15 18:05:23 +0600

    rewrite SectionIntro to SASS and БЭМ.

--- components/SectionIntro/index.js
+++ components/SectionIntro/index.js
@@ -5,26 +5,21 @@ import styles from './styles.module.scss';
 const SectionIntro = () => {
     return (
         <section className={styles.sectionIntro}>
-            <div className={styles.all}>
-                <div className={styles.sectionContainer}>
-
-                    <div className={styles.info}>
+            <div className={styles.sectionIntro__container}>
+                <div className={styles.sectionIntro__sectionContainer}>
+                    <div className={styles.sectionIntro__sectionContainer__info}>
                         Good design meets great user experience
                     </div>
-
-                    <div className={styles.additional}>
+                    <div className={styles.sectionIntro__sectionContainer__additional}>
                         For everyone,from beginners to experts
                     </div>
-
-                    <div className={styles.button}>
-                        <button className={styles.buy}>Buy Digits</button>
-                        <button className={styles.see}>See the Features</button>
+                    <div className={styles.sectionIntro__sectionContainer__button}>
+                        <button className={`${styles.button} ${styles.button_buy}`}>Buy Digits</button>
+                        <button className={`${styles.button} ${styles.button_see}`}>See the Features</button>
                     </div>
-
-                    <div className={styles.graphic}>
+                    <div className={styles.sectionIntro__sectionContainer__graphic}>
                         <img src="/Graphic.svg"/>
                     </div>
-
                 </div>
             </div>
         </section>
